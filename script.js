e08310ac5a22a524eed137b514dd7bf751c5f172
Sanjeet Kumar Verma <vsanjeetkumar951@gmail.com>
2023-08-30 08:29:21 +0530

    changed

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-var istatus=document.querySelector("p");
-var btn1=document.querySelector(".btn-1");
+var istatus = document.querySelector("p");
+var btn1 = document.querySelector(".btn-1");
 
-var check=0;
+var check = 0;
 
-
-
-btn1.addEventListener("click",function() {
-    if(check===0){
-        istatus.innerHTML="Friends"
-        istatus.style.color="red"
-        check=1
-    }else{
-        istatus.innerHTML="Stranger"
-        istatus.style.color="Green"
-        check=0
-    }
-   
-    
-})
-
-    
-    
+btn1.addEventListener("click", function () {
+  if (check === 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "red";
+    btn1.innerHTML="Remove Friend"
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "Green";
+    btn1.innerHTML="Add Friend"
+    check = 0;
+  }
+});
